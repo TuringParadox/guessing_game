f5d95399a5ce3a67d9760e3a9f8390765117893a
TuringParadox <wfscarvalho@gmail.com>
2023-07-27 17:20:24 -0300

    corrigindo print.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ fn main() {
   println!("I'm Online!");
   println!("test thist printd");
   println!("new line added on DEV");
-  println!("another line to prod")
-  println!("isso está funcionando?")
+  println!("another line to prod");
+  println!("isso está funcionando?");
 }
